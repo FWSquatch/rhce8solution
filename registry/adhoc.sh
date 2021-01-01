1c6425542c6913da29e0dd2bdc7d9ee6d4bd7697
Your Name <you@example.com>
2021-01-01 18:34:31 +0000

    Added Workstation

--- registry/adhoc.sh
+++ registry/adhoc.sh
@@ -9,3 +9,4 @@ ansible all -m user -a "name=fred groups=wheel append=true password={{ 'devops' 
 ansible all -m authorized_key -a "user=fred state=present key={{ lookup('file','/home/fred/.ssh/id_rsa.pub') }}" -b -u vagrant
 
 ansible all -m copy -a "content='fred ALL=(ALL) NOPASSWD:ALL' dest=/etc/sudoers.d/fred" -b -u vagrant
+
